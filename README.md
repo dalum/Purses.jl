d2d88aa8ec93ff3b8f9ac6f41962233667dce633
Sakse Dalum <dalum@users.noreply.github.com>
2020-07-24 02:25:26 +0200

    Carry your own cache.

---
+++ README.md
@@ -0,0 +1,48 @@
+# Purses.jl
+
+This package provides a simple and extensible type for wrapping a value that carries a small cache around with it—a purse.  The cache is stored internally as a tuple, and the function for caching the entries are saved as a type parameter.  This trick allows specialising on the function to retrieve the cached value using compile-time constants for indexing into the cache.
+
+# Usage
+
+To cache the result of calling `sum`, `inv∘sum`, and `sqrt∘sum` on a value, we can create a `Purse` as follows:
+```julia
+julia> using Purses
+
+julia> val = rand(10000);
+
+julia> purse = Purse(val, sum, inv∘sum, sqrt∘sum);
+
+```
+To use the cache result of a function, the function must first be registered:
+```julia
+julia> Purses.register(sum, inv∘sum, sqrt∘sum; max_cache=3);
+
+```
+This will define methods for `sum`, `inv∘sum`, and `sqrt∘sum` for all permutations of caches with up to 3 cached items.  The effect of this kind of caching can be quite significant, if the cached value is expensive to compute:
+```julia
+julia> using BenchmarkTools
+
+julia> @btime sum($(Ref(val))[])
+  913.351 ns (0 allocations: 0 bytes)
+5006.181801631625
+
+julia> @btime sum($(Ref(purse))[])
+  1.240 ns (0 allocations: 0 bytes)
+5006.181801631625
+
+julia> @btime (inv∘sum)($(Ref(val))[])
+  914.000 ns (0 allocations: 0 bytes)
+0.00019975303327459624
+
+julia> @btime (inv∘sum)($(Ref(purse))[])
+  1.239 ns (0 allocations: 0 bytes)
+0.00019975303327459624
+
+julia> @btime (sqrt∘sum)($(Ref(val))[])
+  916.853 ns (0 allocations: 0 bytes)
+70.75437655461056
+
+julia> @btime (sqrt∘sum)($(Ref(purse))[])
+  1.241 ns (0 allocations: 0 bytes)
+70.75437655461056
+```
---
+++ src/Purses.jl
@@ -0,0 +1,125 @@
+module Purses
+
+export Purse,
+    cache, value
+
+const _REGISTERED_METHODS = Set()
+
+struct Purse{T,F<:Tuple,S<:Tuple}
+    value::T
+    cache::S
+end
+
+"""
+    Purse(value, fs...)
+
+Return `value` wrapped in a `Purse`.  Each `f` in `fs` can be either a callable or a pair a
+callable and a value.  If `f` is a callable, the cache will automatically be calculated by
+applying each `f` to `value`.  If `f` is a pair, the cache for the first element of the pair
+is set to the value of the second element.  If no `fs` are provided, the cache will be set
+to an empty tuple.
+
+# Examples
+```jldoctest
+julia> Purse(1.0)
+Purse{Float64,Tuple{},Tuple{}}(1.0, ())
+
+julia> Purse(2, inv)
+Purse{Int64,Tuple{typeof(inv)},Tuple{Float64}}(2, (0.5,))
+```
+"""
+function Purse(value, fs...)
+    Fs = map(f -> f isa Pair ? first(f) : f, fs)
+    cache = map(f -> f isa Pair ? last(f) : f(value), fs)
+    return Purse(value, Fs, cache)
+end
+Purse(value::T, ::F, cache::S) where {T,F<:Tuple,S<:Tuple} = Purse{T,F,S}(value, cache)
+
+"""
+    value(x)
+
+If `x` is a `Purse`, return its value field, otherwise return x itself.
+
+# Examples
+```jldoctest
+julia> value(Purse(2.0))
+2.0
+```
+"""
+@inline value(x) = x
+@inline value(x::Purse) = x.value
+
+"""
+    cache(x[, i])
+
+If `x` is a `Purse`, return its cache field, otherwise return an empty tuple.  If the
+optional argument `i` is supplied, return the `i`th stored cache item.
+
+# Examples
+```jldoctest
+julia> purse = Purse(1.0, -);
+
+julia> cache(purse, 1)
+-1.0
+```
+"""
+@inline cache(x) = ()
+@inline cache(x::Purse) = x.cache
+@inline cache(x, i) = nothing
+@inline cache(x::Purse, i) = last(x.cache[i])
+
+"""
+    register(fs...; max_cache=length(fs))
+
+Register each `f` in `fs` as cacheable and return `fs`.  This works by defining methods of
+the type `f(x::Purse{...})` for all `f` in `fs`, and with all permutations of the cache type
+parameter, `typeof(f)`, for cache sizes up to `max_cache`.
+
+!!! note
+    If `f` is already defined for a given cache size, this method will not redefine it.  It
+    is therefore safe to call this method with the same functions multiple times, without
+    incurring method redefinitions.
+
+# Examples
+```jldoctest
+julia> purse = Purse(rand(100), Purses.register(length)...);
+
+julia> length(purse)
+100
+```
+"""
+register(fs...; max_cache=length(fs)) = map(f -> _register(f, max_cache), fs)
+
+function _register(f, max_cache)
+    for cache_size in 1:max_cache
+        expr = _register_impl(f, cache_size)
+        eval(expr)
+    end
+    return f
+end
+
+function _register_impl(f::T, cache_size) where {T}
+    if cache_size == 0
+        T in _REGISTERED_METHODS && return Expr(:block)
+        push!(_REGISTERED_METHODS, T)
+        return :(@inline (f::$T)(x::Purse) = f(value(x)))
+    end
+
+    defs = Expr(:block)
+    for i in 1:cache_size
+        parameters = let cache_size = cache_size
+            ntuple(cache_size) do j
+                i == j ? T : :(<:Any)
+            end
+        end
+
+        parameters in _REGISTERED_METHODS && continue
+
+        push!(_REGISTERED_METHODS, parameters)
+        C = :(Purse{<:Any,<:Tuple{$(parameters...)}})
+        push!(defs.args, :(@inline (::$T)(x::$C) = cache(x, $i)))
+    end
+    return defs
+end
+
+end # module
